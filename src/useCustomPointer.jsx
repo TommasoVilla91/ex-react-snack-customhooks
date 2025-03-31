4b68d904a4accc7e1a6d4057e829ec8f9035a9d2
TommasoVilla91 <tommasovilla@yahoo.it>
2025-03-31 16:44:13 +0200

    feat: snack 3

--- src/useCustomPointer.jsx
+++ src/useCustomPointer.jsx
@@ -1,5 +1,27 @@
-function useCustomPointer() {
+import { useEffect, useState } from "react";
 
+function useCustomPointer(initialValue) {
+    const [customPointer, setCustomPointer] = useState({x: "", y: ""});
+
+    useEffect(() => {
+        const mousePos = (e) => {
+            setCustomPointer({x: e.clientX, y: e.clientY});
+        };
+
+        window.addEventListener("mousemove", mousePos)
+
+    }, []);
+
+    return (
+        <div style={{
+            position: "fixed", 
+            left: `${customPointer.x}px`, 
+            top: `${customPointer.y}px`,
+            cursor: "none"
+        }}>
+            {initialValue}
+        </div>
+    );
 }
 
 export default useCustomPointer;
